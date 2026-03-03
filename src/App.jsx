b8d7149a86b2e2027a287c63a47f2ba278154428
MH-liton-360 <liton72524nk@gmail.com>
2026-03-04 04:01:33 +0600

    Customer_tickets adding page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Customer_Tickets from './Components/Customer_Tickets'
 import Progress_Resolve_Banner from './Components/Progress_Resolve_Banner'
 import Footer from './Shared/Footer'
 import Navbar from './Shared/Navbar'
@@ -11,8 +12,15 @@ function App() {
       <section className=''>
 
         <div className="max-w-7xl mx-auto">
+
+          {/* Navbar  */}
           <Navbar></Navbar>
+
+          {/* Progress_Resolve_Banner  */}
           <Progress_Resolve_Banner></Progress_Resolve_Banner>
+
+          {/* Customer_Tickets  */}
+          <Customer_Tickets></Customer_Tickets>
         </div>
         <Footer></Footer>
       </section>
---
+++ src/Components/Customer_Tickets.jsx
@@ -0,0 +1,14 @@
+
+const Customer_Tickets = () => {
+    return (
+        <section className="">
+
+            <div className="">
+                <h3 className="font-medium text-blue-900">Customer Tickets</h3>
+            </div>
+        </section>
+
+    );
+};
+
+export default Customer_Tickets;
